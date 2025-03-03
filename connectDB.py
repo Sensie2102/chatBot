572b1b1b6a3782946c4c51a042fd60d70325014c
Sensie21 <mafaaz.sabir@gmail.com>
2025-03-03 11:19:50 +0530

    Added security by removing .env file

--- connectDB.py
+++ connectDB.py
@@ -1,9 +1,19 @@
 import pymongo
+import os
 
+CONNECTION_STRING = os.getenv("MONGODB_CONNECTION_STRING")
+DATABASE = os.getenv("DATABASE")
+COLLECTION=os.getenv("COLLECTION")
 
-client = pymongo.MongoClient("mongodb+srv://mafaaz:test123@sensiecluster.sbiuc.mongodb.net/chatbot?retryWrites=true&w=majority")
-db = client["chatbot"]
-collection = db["web"]
-
+def initialize_db():
+    try:
+        client = pymongo.MongoClient(CONNECTION_STRING)
+        db = client[DATABASE]
+        return db[COLLECTION]
+    except ConnectionRefusedError:
+        print("Connection with MongoDB server has been refused, try again.")
+        
+    
 def get_db():
+    collection = initialize_db()
     return collection
